e6b169b977d9405ed45088ba5665da48df982b57
Octavio Elsacte Palma <oelsacte@gmail.com>
2019-10-21 05:55:56 -0500

    Se agrega sass

--- src/app/landing-page/carousel/carousel.component.ts
+++ src/app/landing-page/carousel/carousel.component.ts
@@ -5,7 +5,7 @@ import * as $ from 'jquery';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
 
